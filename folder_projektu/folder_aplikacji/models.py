b3ba4275790f5e39df5fb87fdba18bc27ced5110
JulEng16 <jenglender@st.swsp.edu.pl>
2025-01-19 10:44:29 +0100

    lab_07_feature

--- folder_projektu/folder_aplikacji/models.py
+++ folder_projektu/folder_aplikacji/models.py
@@ -29,7 +29,7 @@ class Person(models.Model):
     shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES, default=SHIRT_SIZES[0][0])
     month_added = models.IntegerField(choices=MONTHS.choices, default=MONTHS.choices[0][0])
     team = models.ForeignKey(Team, null=True, blank=True, on_delete=models.SET_NULL)
-
+    
     def __str__(self):
         return self.name
     
@@ -44,7 +44,7 @@ class Osoba(models.Model):
         nazwisko = models.CharField(max_length=80, blank = False, null = False)
         plec = models.IntegerField(choices=PLCIE.choices, default=PLCIE.choices[2][0])
         stanowisko = models.ForeignKey('Stanowisko', on_delete=models.CASCADE)  #models.CASCADE - usuwa wszytskie osoby ze stanowiska
-        data_dodania = models.DateField(auto_now_add = True, editable = False)
+        data_dodania = models.DateField(auto_now_add = True, editable = False, default=now)
 
         def __str__(self):
             return f'{self.imie} {self.nazwisko}'
